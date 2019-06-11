39c65c41b7f66db72fad8037dd133b87f6f20024
ant-po <anton.ponomarev@gmail.com>
2019-06-11 03:41:07 +0100

    final commit

--- model_main.py
+++ model_main.py
@@ -4,14 +4,13 @@ import build_dataset
 import datetime as time
 
 class Env():
-    # TODO: description here
-    def __init__(self, num_states, epsilon, alpha, gamma, tran_cost):
+    # Define class Env and assign appropriate parameters
+    def __init__(self, num_states, tran_cost, decay, dist_check):
         self.num_states = num_states
         self.curr_state = 0
         self.next_state = np.random.randint(0, self.num_states)
-        self.epsilon = epsilon
-        self.alpha = alpha
-        self.gamma = gamma
+        self.decay = decay
+        self.dist_check = dist_check
         self.time = 0
         self.tran_cost = tran_cost
         self.total_reward = 0
@@ -27,18 +26,19 @@ class Env():
         self.timestep_state.append(self.curr_state)
         self.reward_hist.append(0)
         self.error.append(0)
+        self.dist_hist = [0]
         return
 
     def train(self, data):
-        # TODO: description here
+        # Create/update Knowledge Base and calculate centroids
         # observation : df (r1, r2, r3, time)
         # KB : dict(key: current state, val: dict(key: opt next state, val: df(observations)))
         prior_observation = data.iloc[0:1]
         for t in range(1, data.shape[0]):
             new_observation = data.iloc[t:t + 1]
             for i in range(self.num_states):
-                observation_after_cost = new_observation.iloc[:, :-1] - self.tran_cost
-                observation_after_cost.iloc[:][str(i)] += self.tran_cost
+                observation_after_cost = new_observation.iloc[:, :-1] + self.tran_cost
+                observation_after_cost.iloc[:][str(i)] -= self.tran_cost
                 opt_choice = int(observation_after_cost.idxmax(axis=1))
                 if opt_choice in self.KB[i].keys():
                     self.KB[i][opt_choice] = self.KB[i][opt_choice].append(prior_observation, ignore_index=True)
@@ -49,27 +49,39 @@ class Env():
         return
 
     def updateCentroids(self):
-        # TODO: description here
+        # update centroids given the latest Knowledge Base
         for i in range(self.num_states):
             for j in range(self.num_states):
                 data_slice = self.KB[i][j]
-                x = data_slice.iloc[:, :-1].mul(data_slice.loc[:, 'time'], axis=0).sum(
-                    axis=0) / data_slice.iloc[:, 3].sum()
+                if self.decay:
+                    x = data_slice.iloc[:, :-1].mul(data_slice.loc[:, 'time'], axis=0).sum(
+                        axis=0) / data_slice.loc[:, 'time'].sum()
+                else:
+                    x = data_slice.iloc[:, :-1].sum(axis=0) / data_slice.shape[0]
                 self.centroids[i][j] = pd.DataFrame(x).transpose()
 
     def predict(self, observation):
-        # TODO: description here
+        # make a prediction based on the distance between the centroid and observation
         self.curr_state = self.next_state
-        # find the closest centroid to the observation
+        # find the distance between centroid and the observation
         distances = self.distToCentroid(observation)
-        self.next_state = np.argmin(distances)
+        if self.dist_check:
+            if np.mean(distances) < np.percentile(self.dist_hist, 75):
+                self.next_state = self.curr_state
+            else:
+                self.next_state = np.argmin(distances)
+        else:
+            self.next_state = np.argmin(distances)
+        self.dist_hist.append(np.min(distances))
+            # self.next_state = int(np.random.choice(self.num_states, 1, p=distances.flatten()/sum(distances)))
+
         return
 
     def distToCentroid(self, observation):
-        # TODO: description here
+        # Calculate euclidean distance between the observation and relevant centroid
         distances = []
+        xyz = [str(i) for i in range(self.num_states)]
         for i in range(self.num_states):
-            xyz = ['0', '1', '2']
             dist = np.sum((observation[xyz].values - self.centroids[self.curr_state][i][xyz].values) ** 2, axis=1)
             distances.append(dist)
         return distances
@@ -85,21 +97,20 @@ class Env():
 
 
 if __name__ == "__main__":
-    alpha = 0.1
-    gamma = 0.5
-    epsilon = 1
-    tran_cost = 0
+    tran_cost = -0.0001
+    decay = False
+    dist_check = False
     num_states = 3
-    train_set_size = 1000
-    test_set_size = 1000
+    train_set_size = 260
+    test_set_size = 5000
     output_folder = 'data/processed_data/latest_dataset'
     data = build_dataset.readDataFromCsv(output_folder)
     data.insert(num_states, 'time', range(data.shape[0]))
     data_train = data.iloc[:train_set_size][:]
-    data_test = data.iloc[train_set_size:train_set_size + test_set_size][:]
+    data_test = data.iloc[train_set_size:min(train_set_size+test_set_size, data.shape[0])][:]
 
     # initialize the model
-    model = Env(num_states, epsilon, alpha, gamma, tran_cost)
+    model = Env(num_states, tran_cost, decay, dist_check)
 
     # train the model
     print('Starting initial model training --- ')
@@ -119,7 +130,7 @@ if __name__ == "__main__":
 
         # update prediction track record
         opt_choice = int(new_observation.iloc[:, :-1].idxmax(axis=1))
-        model.error.append(reward - new_observation.iloc[:,0:model.num_states].max(axis=1).item())
+        model.error.append(reward - new_observation.iloc[:, 0:model.num_states].max(axis=1).item())
         model.timestep_state.append(model.next_state)
 
         # recommend next state
@@ -133,36 +144,3 @@ if __name__ == "__main__":
     output_file = 'results/sim_results_' + str(time.datetime.now()) + '.xlsx'
     model.output(data_test, output_file)
     print('--- Model output is complete')
-
-
-
-
-    #
-    #
-    # for episode in range(episodes):
-    #     print('')
-    #     print('--- episode #:{} ---'.format(episode))
-    #     while not model.isEnd:
-    #         model._step()
-    #     print('total reward = {0:.0%}'.format(model.total_reward))
-    #     print('total time spend in states:')
-    #     print('state 1: {0:.0%}'.format(model.timestep_state.count(0)/model.max_steps_train))
-    #     print('state 2: {0:.0%}'.format(model.timestep_state.count(1) / model.max_steps_train))
-    #     print('state 3: {0:.0%}'.format(model.timestep_state.count(2) / model.max_steps_train))
-    #     # print('Final Q:', model.Q.transpose())
-    #     paths[episode] = model.timestep_reward
-    #     perf.append(model.total_reward)
-    #     # plot.figure()
-    #     # plot.plot(range(model.max_steps_train), model.timestep_reward)
-    #     model._reset()
-    # model.train = False
-    # while not model.isEnd:
-    #     model._step()
-    # plot.plot(model.timestep_reward)
-    # # plot.interactive(False)
-    # # plot.show()
-    # # plot.figure()
-    # # plot.plot(range(model.episodes_total), perf)
-    # # plot.show()
-    # res = _collect_total_reward(paths)
-    # print('pause')
